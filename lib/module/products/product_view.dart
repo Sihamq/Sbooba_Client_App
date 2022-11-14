338d3dd51e8ac17d41f06670c2505977a3704d7f
sihamqatani <64069584+sihamqatani@users.noreply.github.com>
2022-11-14 13:52:20 +0300

    first_commit

---
+++ lib/module/products/product_view.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sboba_app_client/module/products/add_product/add_product_view.dart';
+import 'package:sboba_app_client/module/products/details_product/details_product.dart';
+import 'package:sboba_app_client/module/products/product_controller.dart';
+import 'package:sboba_app_client/module/products/widget/add_button.dart';
+import 'package:sboba_app_client/module/products/widget/adding_container.dart';
+import 'package:sboba_app_client/module/products/widget/empty_product.dart';
+import 'package:sboba_app_client/module/products/widget/meal_type_card.dart';
+import 'package:sboba_app_client/module/products/widget/product_cateogry.dart';
+import 'package:sizer/sizer.dart';
+
+import '../my_colors.dart';
+import '../order/widgets/grid_view.dart';
+
+class ProductView extends StatelessWidget {
+  const ProductView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          MaterialButton(
+            color: myGreen,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(25.0))),
+            child: Icon(
+              Icons.search,
+              color: myWhite,
+            ),
+            onPressed: () {},
+          )
+        ],
+        backgroundColor: myGreen,
+        leading: Padding(
+          padding: EdgeInsets.all(1.h),
+          child: Image(image: AssetImage("assets/1.png")),
+        ),
+        elevation: 0,
+      ),
+      body: GetBuilder<ProductController>(
+        init: ProductController(),
+        builder: (controller) => Column(children: [
+          Padding(
+            padding: EdgeInsets.all(2.h),
+            child: Container(
+              width: 48.h,
+              height: 6.h,
+              child: ListView.separated(
+                  separatorBuilder: (context, index) => SizedBox(
+                        width: 1.5.h,
+                      ),
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 3,
+                  itemBuilder: (context, index) {
+                    return ProductCateogry(
+                      x: index,
+                    );
+                  }),
+            ),
+          ),
+          AddingContainer(
+            btnTitle: "Add Product".tr,
+            label: "Add Last".tr,
+            noOfPro: 5,
+            onTap: (() {
+              Get.to(() => AddProduct());
+            }),
+          ),
+          Expanded(
+            child: controller.productItem.isNotEmpty
+                ? GridView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    shrinkWrap: true,
+                    gridDelegate:
+                        const SliverGridDelegateWithMaxCrossAxisExtent(
+                            childAspectRatio: 2 / 2,
+                            maxCrossAxisExtent: 210,
+                            mainAxisSpacing: 10,
+                            crossAxisSpacing: 5),
+                    itemBuilder: ((context, index) => InkWell(
+                        onTap: (() => Get.to(() => const DetailsProduct())),
+                        child: MealCard(index: index))),
+                    itemCount: controller.productItem.length)
+                : EmptyProduct(),
+          ),
+        ]),
+      ),
+    );
+  }
+}
